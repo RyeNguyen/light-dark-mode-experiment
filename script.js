1ed366d30a8a7e77eb25f4a8f3770dff60d5c7aa
RyeNguyen <minhiq149@gmail.com>
2021-07-05 22:25:15 +0700

    Create a basic website with dark/light mode toggle functionality

---
+++ script.js
@@ -0,0 +1,57 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+//Dark or Light Images
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+function toggleDarkLightMode(isLight) {
+    nav.style.backgroundColor = isLight ? 'rgb(0 0 0 / 75%)' : 'rgb(255 255 255 / 75%)';
+    textBox.style.backgroundColor = isLight ? 'rgb(255 255 255 / 75%)' : 'rgb(0 0 0 / 75%)';
+
+    //This is the text
+    toggleIcon.children[0].textContent = isLight ? 'Dark Mode' : 'Light Mode';
+
+    //This is the icon
+    toggleIcon.children[1].classList.replace(
+        isLight ? 'fa-sun' : 'fa-moon',
+        isLight ? 'fa-moon' : 'fa-sun'
+    );
+
+    imageMode(isLight ? 'dark' : 'light');
+}
+
+//Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(true);
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(false);
+    }
+}
+
+//Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+//Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
